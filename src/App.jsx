0ee0fcf0e5d1470fbedaa49cea3dcad81b2b1b4a
unknown <alejandro.flores.a@ucb.edu.bo>
2023-06-13 18:38:45 -0400

    dropzone added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,59 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useDropzone } from 'react-dropzone'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [file, setFile] = useState(null)
+  const [isUploaded, setIsUploaded] = useState(false)
+
+  const onDrop = (acceptedFiles) => {
+    setFile(acceptedFiles[0])
+    setIsUploaded(true)
+  }
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    if (file) {
+      const formData = new FormData()
+      formData.append('file', file)
+
+      fetch('URL_DEL_ENDPOINT', {
+        method: 'POST',
+        body: formData
+      })
+        .then(response => {
+          // Manejar la respuesta del servidor
+        })
+        .catch(error => {
+          // Manejar el error de la solicitud
+        })
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container">
+      <form className="register-file-form" onSubmit={handleSubmit}>
+        <h1>Subir archivo</h1>
+        <div className={`dropzone ${isDragActive ? 'active' : ''}`} {...getRootProps()}>
+          <input {...getInputProps()} />
+          {isUploaded ? (
+            <p>Archivo seleccionado: {file.name}</p>
+          ) : (
+            <p>
+              {isDragActive ? (
+                'Suelta el archivo aquí'
+              ) : (
+                'Arrastra y suelta un archivo aquí, o haz clic para seleccionar un archivo'
+              )}
+            </p>
+          )}
+        </div>
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
   )
 }
 
